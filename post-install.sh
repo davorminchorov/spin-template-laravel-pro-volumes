a85490c7822ad297f10de6dad79bab5ca28a3f56
Jay Rogers <jay@521dimensions.com>
2024-08-02 16:27:09 -0500

    chore: Update post-install.sh to prompt for Let's Encrypt email address

--- post-install.sh
+++ post-install.sh
@@ -329,3 +329,11 @@ done
 clear
 
 process_selections
+
+prompt_and_update_file \
+    --title "Configure Let's Encrypt" \
+    --details "Let's Encrypt requires an email address to send notifications about SSL renewals." \
+    --prompt "Please enter your email" \
+    --file "$project_dir/.infrastructure/conf/traefik/prod/traefik.yml" \
+    --search-default "changeme@example.com" \
+    --success-msg "Updated \".infrastructure/conf/traefik/prod/traefik.yml\" with your email."
