cc84fc90ce49f8527ca1c22950ba5bc7e9461b3f
Jay Rogers <jay@521dimensions.com>
2024-08-05 16:05:59 -0500

    Debug

--- post-install.sh
+++ post-install.sh
@@ -361,6 +361,7 @@ clear
 
 process_selections
 
+set -x
 line_in_file --replace --file "$project_dir/.env" --file "$project_dir/.env.example" "APP_URL" "APP_URL=https://laravel.dev.test"
 
 prompt_and_update_file \
