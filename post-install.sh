29882c6ffbf2c6b84f17c7bcf1be8fad40745339
Jay Rogers <jay@521dimensions.com>
2024-08-06 15:27:08 -0500

    Added callout

--- post-install.sh
+++ post-install.sh
@@ -494,7 +494,7 @@ process_selections
 configure_vite
 line_in_file --replace --file "$project_dir/.env" --file "$project_dir/.env.example" "APP_URL" "APP_URL=https://laravel.dev.test"
 prompt_and_update_file \
-    --title "Configure Let's Encrypt" \
+    --title "🔐 Configure Let's Encrypt" \
     --details "Let's Encrypt requires an email address to send notifications about SSL renewals." \
     --prompt "Please enter your email" \
     --file "$project_dir/.infrastructure/conf/traefik/prod/traefik.yml" \
