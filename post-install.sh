1bd2362634ed6a82a43977eb8da98c62c90b6ed8
Jay Rogers <jay@521dimensions.com>
2024-08-06 13:46:12 -0500

    Fixed service name

--- post-install.sh
+++ post-install.sh
@@ -308,7 +308,7 @@ setup_horizon() {
     cd "$project_dir" || { echo "Failed to change to project directory"; return 1; }
 
     echo "$service_name: Installing Horizon dependencies..."
-    $COMPOSE_CMD run --rm -e COMPOSER_CACHE_DIR=/dev/null -e "SHOW_WELCOME_MESSAGE=false" "$SPIN_PHP_DOCKER_IMAGE" composer --verbose --working-dir=/var/www/html/ require laravel/horizon
+    $COMPOSE_CMD run --rm -e COMPOSER_CACHE_DIR=/dev/null -e "SHOW_WELCOME_MESSAGE=false" php composer --verbose --working-dir=/var/www/html/ require laravel/horizon
 
     cd "$current_dir" || { echo "Failed to return to original directory"; return 1; }
 
