0752afa20d24c6de321b618138200dc0b08d8d15
Jay Rogers <jay@521dimensions.com>
2024-08-16 13:13:18 -0500

    chore: Update post-install.sh to fix GitHub Actions menu display

--- post-install.sh
+++ post-install.sh
@@ -121,8 +121,13 @@ display_feature_menu() {
 display_github_actions_menu() {
     clear
     echo "${BOLD}${YELLOW}Would you like to use GitHub Actions?${RESET}"
-    echo -e "${use_github_actions:+${BOLD}${BLUE}}1) Yes${RESET}"
-    echo -e "${use_github_actions:+${RESET}}${use_github_actions:+${BOLD}${BLUE}}2) No${RESET}"
+    if [ "$use_github_actions" = true ]; then
+        echo -e "${BOLD}${BLUE}1) Yes${RESET}"
+        echo "2) No"
+    else
+        echo "1) Yes"
+        echo -e "${BOLD}${BLUE}2) No${RESET}"
+    fi
     echo "Press a number to select/deselect."
     echo "Press ${BOLD}${BLUE}ENTER${RESET} to continue."
 }
