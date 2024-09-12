5e89da2bfecfa7f48f0413da71d579abd9d0f147
Jay Rogers <jay@521dimensions.com>
2024-09-12 14:29:35 -0500

    Testing spin process

--- install.sh
+++ install.sh
@@ -11,6 +11,8 @@ docker pull "$SPIN_PHP_DOCKER_IMAGE"
 
 laravel_framework_args=("$@")
 
+echo "TEST 👋👋👋"
+
 ###############################################
 # Configure "SPIN_PROJECT_DIRECTORY" variable
 # This variable MUST be the ABSOLUTE path
