e72850e158106c030145c86f85a4d5586f87f727
Jay Rogers <jaydrogers@users.noreply.serversideup.net>
2025-11-05 19:53:44 -0600

    Refactor Dockerfile.php to streamline user and group ID settings for www-data and remove unnecessary PHP-FPM pool configuration commands.

--- template/Dockerfile.php
+++ template/Dockerfile.php
@@ -30,7 +30,7 @@ RUN update-ca-certificates
 
 # Set the user ID and group ID for www-data
 RUN docker-php-serversideup-set-id www-data $USER_ID:$GROUP_ID  && \
-    docker-php-serversideup-set-file-permissions --owner $USER_ID:$GROUP_ID --service nginx
+    docker-php-serversideup-set-file-permissions --owner $USER_ID:$GROUP_ID
 
 # Drop privileges back to www-data    
 USER www-data
@@ -41,12 +41,7 @@ USER www-data
 FROM base AS ci
 
 # Sometimes CI images need to run as root
-# so we set the ROOT user and configure
-# the PHP-FPM pool to run as www-data
 USER root
-RUN echo "" >> /usr/local/etc/php-fpm.d/docker-php-serversideup-pool.conf && \ 
-    echo "user = www-data" >> /usr/local/etc/php-fpm.d/docker-php-serversideup-pool.conf && \
-    echo "group = www-data" >> /usr/local/etc/php-fpm.d/docker-php-serversideup-pool.conf
 
 ############################################
 # Production Image
